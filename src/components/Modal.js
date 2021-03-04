5d4beb6435a8cae24bcd6a3bd2ee58e72423abce
Kim, Sung eun <su79eu7k@gmail.com>
2021-03-04 23:08:02 +0900

    deletion modal work in progress

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,22 +3,62 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Portal from './Portal'
 
-const ModalOverlay = styled.div`
-  display: ${(props) => props.visible ? 'block' : 'none'};
+const ModalWrapper = styled.div`
   box-sizing: border-box;
+  display: ${(props) => (props.visible ? 'block' : 'none')};
   position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
+  z-index: 1000;
+  overflow: auto;
+  outline: 0;
+`
+
+const ModalOverlay = styled.div`
+  box-sizing: border-box;
+  display: ${(props) => (props.visible ? 'block' : 'none')};
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
   background-color: rgba(0, 0, 0, 0.6);
   z-index: 999;
+`
+
+const ModalInner = styled.div`
+  box-sizing: border-box;
+  position: relative;
+  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.5);
+  background-color: #f2f0ea;
+  border-radius: .3rem;
+  top: 50%;
+  transform: translateY(-50%);
+  margin: 0 auto;
+  width: 180px;
+  height: 180px;
+  @media (min-width: 600px) {
+    width: 240px;
+    height: 200px;
+    padding: 1.5rem;
+    font-size: 16px;
+  }
+  padding: 1.5rem;
+  font-size: 12px;
 `
 
 function Modal (props) {
   return (
     <Portal container='modal-root'>
       <ModalOverlay visible={props.visible} />
+      <ModalWrapper visible={props.visible}>
+        <ModalInner>
+          <p>Sibling articles of this day will be deleted as well...!</p>
+          <p>Are you sure about this?</p>
+        </ModalInner>
+      </ModalWrapper>
     </Portal>
   )
 }
